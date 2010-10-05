9c4228e6180e77ace3e4ba527e3f384ef5a9aacf
sammy <sammy@fiteen.(none)>
2010-10-04 18:01:04 -0700

    doc examples/action_list

--- examples/action_list.rb
+++ examples/action_list.rb
@@ -10,16 +10,21 @@ end
 
 r.create_table("actionlist", "(id INT PRIMARY KEY, user_id INT, timestamp INT, action TEXT)")
 
+#USER 1
 r.insert("actionlist", "(1,1,12345,account created")
 r.insert("actionlist", "(2,1,12346,first login")
 r.insert("actionlist", "(3,1,12347,became paid member")
 r.insert("actionlist", "(4,1,12348,posted picture")
 r.insert("actionlist", "(5,1,12349,filled in profile")
 r.insert("actionlist", "(6,1,12350,signed out")
+
+#USER 2
 r.insert("actionlist", "(7,2,22345,signed in")
 r.insert("actionlist", "(8,2,22346,updated picture")
 r.insert("actionlist", "(9,2,22347,checked email")
 r.insert("actionlist", "(10,2,22348,signed in")
+
+#USER 3
 r.insert("actionlist", "(11,3,32348,signed in")
 r.insert("actionlist", "(12,3,32349,contacted customer care")
 r.insert("actionlist", "(13,3,32350,upgraded account")
